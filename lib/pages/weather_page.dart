4ecab82688fb6f8153936c156d5b3c916380f8a1
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2024-03-05 17:47:47 +0530

    "First Run Faced android error"

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/model/weather_model.dart';
 import 'package:weather_app/service/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -10,8 +11,40 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   final _weatherService = WeatherService();
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "loading city name"),
+            Text('${_weather?.temprature.round()}C')
+          ],
+        ),
+      ),
+    );
   }
 }
